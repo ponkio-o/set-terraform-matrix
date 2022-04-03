c8753c90b1727ef8a82110782994003a6022f421
ponkio-o <29038315+ponkio-o@users.noreply.github.com>
2022-04-03 15:31:49 +0900

    FIX: move inputs to environment variables

--- main.sh
+++ main.sh
@@ -7,40 +7,40 @@ exist_production_label=$(cat $CI_INFO_TEMP_DIR/labels.txt | grep "target:product
 
 if [ $exist_all_label = "0" ]; then
   echo "Deploy all environment"
-  list=$(jq -c '.develop + .staging + .production' ${{ inputs.deploy_target_file }})
+  list=$(jq -c '.develop + .staging + .production' ${DEPLOY_TARGET_FILE})
   echo "::set-output name=value::${list}"
 elif [ $exist_develop_label = "0" -a $exist_staging_label = "0" -a $exist_production_label = "0" ]; then
   echo "Deploy all environment"
-  list=$(jq -c '.develop + .staging + .production' ${{ inputs.deploy_target_file }})
+  list=$(jq -c '.develop + .staging + .production' ${DEPLOY_TARGET_FILE})
   echo "::set-output name=value::${list}"
 elif [ $exist_develop_label = "0" -a $exist_staging_label = "0" ]; then
   echo "Deploy dev & stg"
-  list=$(jq -c '.develop + .staging' ${{ inputs.deploy_target_file }})
+  list=$(jq -c '.develop + .staging' ${DEPLOY_TARGET_FILE})
   echo "::set-output name=value::${list}"
 elif [ $exist_develop_label = "0" -a $exist_production_label = "0" ]; then
   echo "Deploy dev & prod"
-  list=$(jq -c '.develop + .production' ${{ inputs.deploy_target_file }})
+  list=$(jq -c '.develop + .production' ${DEPLOY_TARGET_FILE})
   echo "::set-output name=value::${list}"
 elif [ $exist_staging_label = "0" -a $exist_production_label = "0" ]; then
   echo "Deploy stg & prod"
-  list=$(jq -c '.staging + .production' ${{ inputs.deploy_target_file }})
+  list=$(jq -c '.staging + .production' ${DEPLOY_TARGET_FILE})
   echo "::set-output name=value::${list}"
 elif [ $exist_develop_label = "0" ]; then
   echo "Deploy dev"
-  list=$(jq -c '.develop' ${{ inputs.deploy_target_file }})
+  list=$(jq -c '.develop' ${DEPLOY_TARGET_FILE})
   echo "::set-output name=value::${list}"
 elif [ $exist_staging_label = "0" ]; then
   echo "Deploy stg"
-  list=$(jq -c '.staging' ${{ inputs.deploy_target_file }})
+  list=$(jq -c '.staging' ${DEPLOY_TARGET_FILE})
   echo "::set-output name=value::${list}"
 elif [ $exist_production_label = "0" ]; then
   echo "Deploy prod"
-  list=$(jq -c '.production' ${{ inputs.deploy_target_file }})
+  list=$(jq -c '.production' ${DEPLOY_TARGET_FILE})
   echo "::set-output name=value::${list}"
 else
-  export PR_NUMBER=$(echo ${{ inputs.pr_number }})
-  export DEPLOY_TARGET_FILE=$(echo ${{ inputs.deploy_target_file }})
-  github-comment post -org ${GITHUB_REPOSITORY%/*} -repo ${GITHUB_REPOSITORY#*/} --config ${{ github.action_path }}/.github-comment.yml -pr ${PR_NUMBER} -k select-label -var DEPLOY_TARGET_FILE:${DEPLOY_TARGET_FILE}
+  export PR_NUMBER=$(echo ${PR_NUMBER})
+  export DEPLOY_TARGET_FILE=$(echo ${DEPLOY_TARGET_FILE})
+  github-comment post -org ${GITHUB_REPOSITORY%/*} -repo ${GITHUB_REPOSITORY#*/} --config ${GITHUB_ACTION_PATH}/.github-comment.yml -pr ${PR_NUMBER} -k select-label -var DEPLOY_TARGET_FILE:${DEPLOY_TARGET_FILE}
   exit 1
 fi
 echo "OUTPUT: $list"
